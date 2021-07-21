b3979adc548432151f30aa2cb6de9d5ab53b49ba
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2021-07-21 21:45:26 +0200

    0013 Update linting (2 warnings remaining)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RegionsOtwModule } from './modules/regions-otw-client/regions-otw.module';
 import { PortalHomeComponent } from './components/portal-home/portal-home.component';
 
 const routes: Routes = [
   {
     path: 'regions',
-    loadChildren: () => import('./modules/regions-otw-client/regions-otw.module').then(module => {
+    loadChildren: (): Promise<RegionsOtwModule> => import('./modules/regions-otw-client/regions-otw.module').then(module => {
       return module.RegionsOtwModule
     }),
     data: {
