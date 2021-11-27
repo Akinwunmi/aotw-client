469a9d73beac4704553cdd1007788bc5fae0353d
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2021-11-27 11:53:39 +0100

    0034 Add football-otw routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // modules
+import { FootballOtwModule } from './football-otw-client/football-otw.module';
 import { RegionsOtwModule } from './regions-otw-client/regions-otw.module';
 // components
 import { PortalHomeComponent } from './portal-home/portal-home.component';
 
 const routes: Routes = [
+  {
+    path: 'football',
+    loadChildren: (): Promise<FootballOtwModule> => import('./football-otw-client/football-otw.module').then(module => {
+      return module.FootballOtwModule
+    }),
+    data: {
+      title: 'Football'
+    }
+  },
   {
     path: 'regions',
     loadChildren: (): Promise<RegionsOtwModule> => import('./regions-otw-client/regions-otw.module').then(module => {
