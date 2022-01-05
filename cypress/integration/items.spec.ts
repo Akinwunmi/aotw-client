3ea4b1924cab9be50c6fa403db893e1bf25153cb
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2022-01-05 11:03:52 +0100

    0046 First e2e test

---
+++ cypress/integration/items.spec.ts
@@ -0,0 +1,27 @@
+// Copyright 2022,
+// Jurrit van der Ploeg
+
+describe('Regions', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    // cy.pause();
+
+    cy.contains('regions').click();
+
+    cy.url()
+      .should('include', '/regions/discover');
+    cy.get('li')
+      .should('have.length', 6);
+  });
+
+  it('navigates to Saba', () => {
+    cy.contains('North America').click();
+    cy.contains('Saba').click();
+  });
+
+  it('navigates to Baden-Württemberg', () => {
+    cy.contains('Europe').click();
+    cy.contains('Germany').click();
+    cy.contains('Baden-Württemberg').click();
+  });
+});
